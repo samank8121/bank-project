168355a3c4f6e373e369fedeed98b0cf1d937de4
saman <saman.k8121@gmail.com>
2024-12-04 10:04:31 +0300

    fix: check iban in create time

--- src/app/api/account/create/route.ts
+++ src/app/api/account/create/route.ts
@@ -1,10 +1,16 @@
 import { getMessage } from '@/messages';
 import prisma from '@/shared/data/prisma';
+import { isValidIBAN } from '@/shared/utils/iban';
 import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
   const { iban, initialBalance } = await req.json();
-
+  if (!isValidIBAN(iban)) {
+    return NextResponse.json(
+      { success: false, error: getMessage('account','invalidIban') },
+      { status: 400 }
+    );
+  }
   try {
     const account = await prisma.account.create({
       data: {
