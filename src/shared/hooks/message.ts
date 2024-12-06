5451977fad54cc79b04887cb93d614577eed4e73
saman <saman.k8121@gmail.com>
2024-12-06 11:17:39 +0300

    refactor: simple useMessage hook

--- src/shared/hooks/message.ts
+++ src/shared/hooks/message.ts
@@ -1,10 +1,6 @@
 'use client';
-import { getMessage, MessageKey, NestedMessageKey } from '@/messages';
+import { getMessage } from '@/messages';
 
 export function useMessage() {
-  return <T extends MessageKey>(
-    category: T,
-    key: NestedMessageKey<T>,
-    params?: Record<string, string | number>
-  ) => getMessage(category, key, params);
+  return getMessage;
 }
